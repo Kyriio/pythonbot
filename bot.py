d727af1923f2ace5ab841883b58adef693a72c0a
Kyriio <57706665+Kyriio@users.noreply.github.com>
2021-12-26 00:51:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,6 @@ import os
 from discord.ext import commands
 import dotenv
 import requests
-from config import api_lol, TOKEN
-
-
-dotenv.load_dotenv(dotenv_path="./.env/config")#où se trouve le token
 
 
 intents = discord.Intents(messages=True,guilds=True,reactions=True,members=True,presences=True)
@@ -67,7 +63,7 @@ async def help(ctx):
 @bot.command()
 async def lol_profile(ctx,name):
     
-    url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key='+api_lol
+    url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key='+os.environ['api_lol']
     response = requests.get(url)
     data = response.json()
     name = data['name']
@@ -94,12 +90,12 @@ def romain_to_int(num: str) -> int:
 #donne le rank de l'utilisateur
 @bot.command()
 async def rank_lol(ctx,name):
-    url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key='+api_lol
+    url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key='+os.environ['api_lol']
     response = requests.get(url)
     data = response.json()
 
     id_joueur = data['id']
-    url_rank = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+id_joueur+'?api_key='+api_lol
+    url_rank = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+id_joueur+'?api_key='+os.environ['api_lol']
     response_rank= requests.get(url_rank)
 
     data_rank=response_rank.json()
@@ -147,4 +143,4 @@ async def random_meme(ctx):
 
 
 
-bot.run(TOKEN)
+bot.run(os.environ['TOKEN'])
